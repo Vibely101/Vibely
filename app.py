28f55382331ebfeb4602ef1fb885baf4b952d31d
Chiwenite Chilz <chilzchiwenite@gmail.com>
2026-07-08 02:45:03 -0700

    deploy update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config["MAIL_SERVER"] = "smtp.sendgrid.net"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
-app.config["MAIL_USERNAME"] = "acdd539fc0f4c1d5ca2f61f256a5888c"
+app.config["MAIL_USERNAME"] = "apikey"
 app.config["MAIL_PASSWORD"] = os.environ.get("SENDGRID_API_KEY")
 app.config["MAIL_DEFAULT_SENDER"] = "qevra101@gmail.com"
 
