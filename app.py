14e52f1cf75d5c647820095b48e1d412994d8367
Chiwenite Chilz <chilzchiwenite@gmail.com>
2026-07-08 01:38:49 -0700

    Fix mail config and forgot password

--- app.py
+++ app.py
@@ -29,9 +29,10 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
-app.config["qevra101@gmail.com"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["qevra101@gmail.com"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
 
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
